03c97acf4ff359a0d20013720e7fe4a3c80fc3fd
Eudes <Eudes@DESKTOP-7GCEH41.Home>
2018-07-06 19:49:41 -0300

    Add include de contatos

--- src/main/java/com/br/marte/api/resource/Wstatusresource.java
+++ src/main/java/com/br/marte/api/resource/Wstatusresource.java
@@ -2,8 +2,12 @@ package com.br.marte.api.resource;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +26,15 @@ public class Wstatusresource {
 		
 		return ws.findAll();
 	}
+	
+	
+	@PostMapping
+	public Wstatus statusAdd(@Valid @RequestBody Wstatus status) {
+		
+		return ws.save(status);
+	}
+	
+	
+	
 
 }
